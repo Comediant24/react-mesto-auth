bb87aac3edc7c71caed2bd522e5c924ea1d08708
Iurii Bairamukov <juribai@mail.ru>
2020-11-09 20:46:48 +0700

    feat: изменить Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../images/logo.svg';
 
 function Header() {
   return (
     <header className="header">
-      <a href="./" className="header__link">
+      <Link to="./" className="header__link">
         <img src={logo} alt="Логотип сайта" className="header__logo" />
-      </a>{' '}
+      </Link>
+      <ul className="header__auth-list">
+        <li className="header__auth-item">
+          <Link
+            to="./sign-up"
+            className="link header__link header__link_sign-up"
+          >
+            Регистрация
+          </Link>
+        </li>
+        <li className="header__auth-item">
+          <Link
+            to="./sign-in"
+            className="link header__link header__link_sign-in"
+          >
+            Вход
+          </Link>
+        </li>
+      </ul>
     </header>
   );
 }
