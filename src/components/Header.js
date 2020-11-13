825e06a653fe886860f333ded2eb57a5986e6af9
Iurii Bairamukov <juribai@mail.ru>
2020-11-13 17:43:00 +0700

    feat: доработать стили Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,49 +2,46 @@ import React from 'react';
 import { Link, Route } from 'react-router-dom';
 import logo from '../images/logo.svg';
 
-function Header({ loggedIn, userData, onClick }) {
+function Header({ loggedIn, userEmail, onClick }) {
   return (
     <header className="header">
       <Link to="/" className="header__link">
         <img src={logo} alt="Логотип сайта" className="header__logo" />
       </Link>
-      <ul className="header__auth-list">
-        {loggedIn ? (
-          <>
-            <li className="header__auth-item">{userData.email}</li>
-            <li className="header__auth-item">
-              <button
-                onClick={onClick}
-                className="link header__link button header__button header__button_signOut"
-              >
-                Выход
-              </button>
-            </li>
-          </>
-        ) : (
-          <>
-            <Route path="/sign-in">
-              <li className="header__auth-item">
-                <Link
-                  to="/sign-up"
-                  className="link header__link header__link_sign-up"
-                >
-                  Регистрация
-                </Link>
-              </li>
-            </Route>
-            <Route path="/sign-up">
-              <li className="header__auth-item">
-                <Link
-                  to="/sign-in"
-                  className="link header__link header__link_sign-in"
-                >
-                  Вход
-                </Link>
-              </li>
-            </Route>
-          </>
-        )}
+      <ul className="header__nav">
+        <Route exact path="/">
+          <li className="header__nav-item">
+            <p className="header__user-email">{userEmail}</p>
+          </li>
+          <li className="header__nav-item">
+            <button
+              onClick={onClick}
+              className="link header__link button header__button header__button_signOut"
+            >
+              Выход
+            </button>
+          </li>
+        </Route>
+        <Route path="/sign-in">
+          <li className="header__nav-item">
+            <Link
+              to="/sign-up"
+              className="link header__link header__link_sign-up"
+            >
+              Регистрация
+            </Link>
+          </li>
+        </Route>
+        <Route path="/sign-up">
+          <li className="header__nav-item">
+            <Link
+              to="/sign-in"
+              className="link header__link header__link_sign-in"
+            >
+              Вход
+            </Link>
+          </li>
+        </Route>
       </ul>
     </header>
   );
